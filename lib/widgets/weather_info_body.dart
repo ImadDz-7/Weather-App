52df3fd616c6509d9ff25846e8eb55842e5f481d
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-07-06 18:03:16 +0000

    Weather info almost done

--- lib/widgets/weather_info_body.dart
+++ lib/widgets/weather_info_body.dart
@@ -11,7 +11,7 @@ class WeatherInfoBody extends StatelessWidget {
         const Text(
           'Algeria',
           style: TextStyle(
-            fontSize: 30,
+            fontSize: 32,
             fontWeight: FontWeight.bold,
           ),
         ),
@@ -21,17 +21,35 @@ class WeatherInfoBody extends StatelessWidget {
             fontSize: 18,
           ),
         ),
+        const SizedBox(height: 36),
         Row(
-          children: const [
-            Image(
-              image: AssetImage(''),
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            const SizedBox(width: 12),
+            const Image(
+              image: AssetImage('assets/cloudy.png'),
             ),
-            Text(
+            const Text(
               '28',
-              style: TextStyle(fontSize: 22),
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24,),
             ),
-            
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text('maxTemp: 30'),
+                Text('minTemp: 17'),
+              ],
+            ),
+            const SizedBox(width: 12),
           ],
+        ),
+        const SizedBox(height: 36),
+        const Text(
+          'Sunny',
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ],
     );
