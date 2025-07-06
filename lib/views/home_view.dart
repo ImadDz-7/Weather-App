cc1c84733b95df7c3a9ec5f475be31e30cf4796c
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-07-06 14:36:23 +0000

    Create WeatherInfoBody widget

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:weather/widgets/no_weather_body.dart';
+import 'package:weather/widgets/weather_info_body.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -16,7 +17,7 @@ class HomeView extends StatelessWidget {
           ),
         ],
       ),
-      body: const NoWeatherBody(),
+      body: const WeatherInfoBody(),
     );
   }
 }
