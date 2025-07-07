669618a82c62e46a0e64af886622a443e47d0edf
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-07-07 14:39:20 +0000

    Weather info body UI done

--- lib/widgets/weather_info_body.dart
+++ lib/widgets/weather_info_body.dart
@@ -15,23 +15,27 @@ class WeatherInfoBody extends StatelessWidget {
             fontWeight: FontWeight.bold,
           ),
         ),
+        const SizedBox(height: 5),
         const Text(
           'Updated at : 15:02',
           style: TextStyle(
             fontSize: 18,
           ),
         ),
-        const SizedBox(height: 36),
+        const SizedBox(height: 46),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            const SizedBox(width: 12),
+            const SizedBox(width: 10),
             const Image(
               image: AssetImage('assets/cloudy.png'),
             ),
             const Text(
               '28',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 24,),
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 28,
+              ),
             ),
             Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -40,10 +44,10 @@ class WeatherInfoBody extends StatelessWidget {
                 Text('minTemp: 17'),
               ],
             ),
-            const SizedBox(width: 12),
+            const SizedBox(width: 10),
           ],
         ),
-        const SizedBox(height: 36),
+        const SizedBox(height: 46),
         const Text(
           'Sunny',
           style: TextStyle(
