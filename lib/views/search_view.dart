08ecd63e2723d18966cd2b43ece6fb9021e034f6
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-07-07 15:42:28 +0000

    Adding AppBar

--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -5,6 +5,10 @@ class SearchView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Search a City'),
+      ),
+    );
   }
 }
