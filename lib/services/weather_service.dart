bd7972c9a08d1d9fbb7ab5785f25b49b3c1d69fc
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-07-09 15:04:27 +0000

    Adding getCurrentWeather methode to make the request using dio

--- lib/services/weather_service.dart
+++ lib/services/weather_service.dart
@@ -1,3 +1,29 @@
-class WeatherService{
+import 'package:dio/dio.dart';
+import 'package:weather/models/weather_model.dart';
 
+class WeatherService {
+  final Dio dio;
+
+  WeatherService(this.dio);
+
+  Future<WeatherModel> getCurrentWeather({required String cityName}) async {
+    try {
+      Response response = await dio.get(
+          'https://api.weatherapi.com/v1/forecast.json?key=f8ff0cb220fb438dbb0175745242101&q=$cityName&days=5');
+      Map<String, dynamic> jsonData = response.data;
+
+      WeatherModel weatherModel = WeatherModel.fromJson(jsonData);
+
+      return weatherModel;
+    } catch (e) {
+      return WeatherModel(
+        cityName: '',
+        date: '',
+        temp: 0,
+        maxTemp: 0,
+        minTemp: 0,
+        weatherCondition: '',
+      );
+    }
+  }
 }
