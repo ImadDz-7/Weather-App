a1ab42a394b7e369609f40fe586d8b9dab2d4ff0
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-07-09 13:44:23 +0000

    Add factory constructor

--- lib/models/weather_model.dart
+++ lib/models/weather_model.dart
@@ -16,4 +16,16 @@ class WeatherModel {
     required this.minTemp,
     required this.weatherCondition,
   });
+
+  factory WeatherModel.fromJson(json) {
+    return WeatherModel(
+      cityName: json['location']['name'],
+      date: json['current']['last_updated'],
+      image: json['forecast']['forecastday'][0]['day']['condition']['icon'],
+      temp: json['forecast']['forecastday'][0]['day']['avgtemp_c'],
+      maxTemp: json['forecast']['forecastday'][0]['day']['maxtemp_c'],
+      minTemp: json['forecast']['forecastday'][0]['day']['mintemp_c'],
+      weatherCondition: json['forecast']['forecastday'][0]['day']['condition']['text'],
+    );
+  }
 }
