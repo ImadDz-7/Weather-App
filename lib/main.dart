e9511ca6b963a54050c16f4ddb3650f4c7d502e9
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-07-10 22:33:51 +0000

    Trigger request & test response

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:weather/views/home_view.dart';
 
 void main() {
+  HttpOverrides.global = MyHttpOverrides();
   runApp(const WeatherApp());
+}
+
+class MyHttpOverrides extends HttpOverrides {
+  @override
+  HttpClient createHttpClient(SecurityContext? context) {
+    return super.createHttpClient(context)
+      ..badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+  }
 }
 
 class WeatherApp extends StatelessWidget {
--- lib/views/search_view.dart
+++ lib/views/search_view.dart
@@ -1,6 +1,9 @@
 import 'dart:developer';
 
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:weather/models/weather_model.dart';
+import 'package:weather/services/weather_service.dart';
 
 class SearchView extends StatelessWidget {
   const SearchView({super.key});
@@ -15,7 +18,10 @@ class SearchView extends StatelessWidget {
         padding: const EdgeInsets.symmetric(horizontal: 16),
         child: Center(
           child: TextField(
-            onChanged: (value){
+            onSubmitted: (value) async {
+              WeatherModel weatherModel = await WeatherService(Dio())
+                  .getCurrentWeather(cityName: value);
+              log(weatherModel.cityName);
             },
             decoration: InputDecoration(
               contentPadding: const EdgeInsets.symmetric(
