d6fd744685b2ca9054d43614b18781f34cec7e6d
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-07-14 14:10:35 +0000

    Provide GetWeatherCubit using BlocProvider

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather/cubits/get_weather_cubit/get_weather_cubit.dart';
 import 'package:weather/views/home_view.dart';
 
 void main() {
@@ -22,9 +24,12 @@ class WeatherApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeView(),
+    return BlocProvider(
+      create: (context) => GetWeatherCubit(),
+      child: const MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: HomeView(),
+        ),
     );
   }
 }
