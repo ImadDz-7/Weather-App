1a1013421f79e20905b53470f3f02853ab1ff900
ImadDz-7 <imadbouamrirene97@gmail.com>
2025-07-14 14:39:17 +0000

    Integrate Cubit using (BlocBuilder) in HomeView

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather/cubits/get_weather_cubit/get_weather_cubit.dart';
+import 'package:weather/cubits/get_weather_cubit/get_weather_states.dart';
 import 'package:weather/views/search_view.dart';
 import 'package:weather/widgets/no_weather_body.dart';
 import 'package:weather/widgets/weather_info_body.dart';
@@ -26,7 +29,17 @@ class HomeView extends StatelessWidget {
           ),
         ],
       ),
-      body: const NoWeatherBody(),
+      body: BlocBuilder<GetWeatherCubit, WeatherState>(
+        builder: (context, state) {
+          if (state is NoWeatherInitialState) {
+            return const NoWeatherBody();
+          } else if (state is WeatherLoadedState) {
+            return const WeatherInfoBody();
+          } else {
+            return const Text('Oops there was an error!');
+          }
+        },
+      ),
     );
   }
 }
